c495e1917adeb924bc928cb40f43a607ae2c188d
dbhuller <dbhuller@mail.sfsu.edu>
2020-12-11 22:53:06 -0800

    added main page components and initial styling

---
+++ blog-app/src/pages/AboutPage.js
@@ -0,0 +1,53 @@
+import React from "react";
+
+const AboutPage = () => (
+  <>
+    <h1>About Me</h1>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+      tempor incididunt ut labore et dolore magna aliqua. Quis vel eros donec ac
+      odio tempor orci dapibus ultrices. Amet commodo nulla facilisi nullam
+      vehicula ipsum. Tincidunt vitae semper quis lectus nulla at volutpat diam
+      ut. Turpis egestas pretium aenean pharetra magna ac placerat. Tincidunt
+      arcu non sodales neque. Morbi tristique senectus et netus et malesuada
+      fames ac. Sollicitudin tempor id eu nisl nunc. Amet nisl suscipit
+      adipiscing bibendum est ultricies integer quis auctor. In eu mi bibendum
+      neque egestas.
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+      tempor incididunt ut labore et dolore magna aliqua. Quis vel eros donec ac
+      odio tempor orci dapibus ultrices. Amet commodo nulla facilisi nullam
+      vehicula ipsum. Tincidunt vitae semper quis lectus nulla at volutpat diam
+      ut. Turpis egestas pretium aenean pharetra magna ac placerat. Tincidunt
+      arcu non sodales neque. Morbi tristique senectus et netus et malesuada
+      fames ac. Sollicitudin tempor id eu nisl nunc. Amet nisl suscipit
+      adipiscing bibendum est ultricies integer quis auctor. In eu mi bibendum
+      neque egestas.
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+      tempor incididunt ut labore et dolore magna aliqua. Quis vel eros donec ac
+      odio tempor orci dapibus ultrices. Amet commodo nulla facilisi nullam
+      vehicula ipsum. Tincidunt vitae semper quis lectus nulla at volutpat diam
+      ut. Turpis egestas pretium aenean pharetra magna ac placerat. Tincidunt
+      arcu non sodales neque. Morbi tristique senectus et netus et malesuada
+      fames ac. Sollicitudin tempor id eu nisl nunc. Amet nisl suscipit
+      adipiscing bibendum est ultricies integer quis auctor. In eu mi bibendum
+      neque egestas.
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+      tempor incididunt ut labore et dolore magna aliqua. Quis vel eros donec ac
+      odio tempor orci dapibus ultrices. Amet commodo nulla facilisi nullam
+      vehicula ipsum. Tincidunt vitae semper quis lectus nulla at volutpat diam
+      ut. Turpis egestas pretium aenean pharetra magna ac placerat. Tincidunt
+      arcu non sodales neque. Morbi tristique senectus et netus et malesuada
+      fames ac. Sollicitudin tempor id eu nisl nunc. Amet nisl suscipit
+      adipiscing bibendum est ultricies integer quis auctor. In eu mi bibendum
+      neque egestas.
+    </p>
+  </>
+);
+
+export default AboutPage;
