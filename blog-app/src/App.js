0e6857d30c0385ee81410729129167627dcf0e4d
dbhuller <dbhuller@mail.sfsu.edu>
2020-12-11 23:21:11 -0800

    added navbar and dummy article data and functionality to view articles through typing article name into url

--- blog-app/src/App.js
+++ blog-app/src/App.js
@@ -1,19 +1,21 @@
 import "./App.css";
 import HomePage from "./pages/HomePage";
-import ArticlePage from "./pages/AboutPage";
+import ArticlePage from "./pages/ArticlePage";
 import ArticlesList from "./pages/ArticlesList";
 import AboutPage from "./pages/AboutPage";
+import NavBar from "./NavBar";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
 function App() {
   return (
     <Router>
       <div className="App">
+        <NavBar />
         <div id="page-body">
           <Route path="/" component={HomePage} exact />
           <Route path="/about" component={AboutPage} />
           <Route path="/articles-list" component={ArticlesList} />
-          <Route path="/article" component={ArticlePage} />
+          <Route path="/article/:name" component={ArticlePage} />
         </div>
       </div>
     </Router>
